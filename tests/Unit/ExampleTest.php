04fcdef8d96e6bad8e4b6d59796065a8ba0160ec
Vlad Osmianski <vlaosm@gmail.com>
2023-03-13 21:58:20 +0200

    Pest added

--- tests/Unit/ExampleTest.php
+++ tests/Unit/ExampleTest.php
@@ -2,15 +2,6 @@
 
 namespace Tests\Unit;
 
-use PHPUnit\Framework\TestCase;
-
-class ExampleTest extends TestCase
-{
-    /**
-     * A basic test example.
-     */
-    public function test_that_true_is_true(): void
-    {
-        $this->assertTrue(true);
-    }
-}
+it('knows what is true', function() {
+    $this->assertTrue(true);
+});
