5302efbab43b87ed89036426a08d42aeebbd73b5
Vlad Osmianski <vlaosm@gmail.com>
2023-03-14 15:42:27 +0200

    `accounts`, `pages`, `nodes` `slugs` table

--- database/migrations/2023_03_14_132851_create_nodes_table.php
+++ database/migrations/2023_03_14_132851_create_nodes_table.php
@@ -11,13 +11,15 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('nodes', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
+            $table->string('type')->nullable();
+            $table->string('slug')->nullable()->index();
+            $table->foreignId('page_id')
+                ->nullable()
+                ->constrained()
+                ->cascadeOnDelete();
+            $table->json('data')->nullable();
             $table->timestamps();
         });
     }
@@ -27,6 +29,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('nodes');
     }
 };
---
+++ database/migrations/2023_03_14_133408_create_slugs_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('slugs', function (Blueprint $table) {
+            $table->id();
+            $table->string('type')->nullable();
+            $table->string('slug')->nullable()->index();
+            $table->foreignId('page_id')
+                ->nullable()
+                ->constrained()
+                ->cascadeOnDelete();
+            $table->foreignId('account_id')
+                ->nullable()
+                ->constrained()
+                ->cascadeOnDelete();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('slugs');
+    }
+};
